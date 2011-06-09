3d59220db444f09839ecad6006935c4c877ba314
Zhiyong Wu <wuzhy@vnet.linux.ibm.com>
2011-06-09 17:41:51 +0800

    Modify the computing method for I/O rate at runtime.
    Signed-off-by: Zhiyong Wu <wuzhy@vnet.linux.ibm.com>

--- client/tests/kvm/tests/disk_io_limits.py
+++ client/tests/kvm/tests/disk_io_limits.py
@@ -27,35 +27,39 @@ met.
     # The default unit of io_limits is MB/s
     io_limits = params.get("io_limits", "100 MB/s")
     io_limits = re.findall(r'[0-9]* MB\/s', io_limits)
+    if not io_limits:
+        raise error.TestFail('Fail to get I/O rate limit.')
+
     io_limits = string.join(io_limits)
     io_limits = io_limits.replace(' MB/s','')
     io_limits = float(io_limits)
 
     try:
         output = session.cmd_output('dd if=/dev/vda of=/dev/null bs=256K count=128 iflag=direct')
-        bps_bytes = re.findall(r'[0-9]*[.][0-9]* [GM]B\/s', output)
+        bps_bytes = re.findall(r'[0-9]* bytes', output)
+        if not bps_bytes:
+           raise error.TestFail('Fail to get I/O bytes at runtime.')
+
+        bps_seconds = re.findall(r'[0-9]*[.][0-9]* s', output)
+        if not bps_seconds:
+           raise error.TestFail('Fail to get I/O seconds at runtime.')
+
         bps_bytes = string.join(bps_bytes)
-        G_byte = 0
-        B_byte = 0
-        match = re.match(r'GB\/s', bps_bytes) 
-        if match:
-           G_byte = 1
-        match = re.match(r'B\/s', bps_bytes)
-        if match:
-           B_byte = 1
-        bps_bytes = bps_bytes.replace(' GB/s','')
-        bps_bytes = bps_bytes.replace(' MB/s','')
-        bps_bytes = bps_bytes.replace(' B/s','')
+        bps_bytes = bps_bytes.replace(' bytes','')
         bps_bytes = float(bps_bytes)
-        if G_byte == 1:
-           bps_bytes = bps_bytes * 1024
-        elif B_byte == 1:
-           bps_bytes = bps_bytes / 1024
 
-        logging.debug("io_limits = %d, bps_bytes = %d", io_limits, bps_bytes)
+        bps_seconds = string.join(bps_seconds)
+        bps_seconds = bps_seconds.replace(' s','')
+        bps_seconds = float(bps_seconds)
+        if bps_seconds == 0:
+           raise error.TestFail('The I/O seconds at runtime is zero.')
 
-        if bps_bytes > io_limits:
-           raise error.TestFail('Throughput has exceeded threshold ')
+        logging.debug("bps_bytes = %d, bps_seconds = %f", bps_bytes, bps_seconds)
+        bps_iorate = (bps_bytes / (1024 * 1024)) / bps_seconds
+
+        logging.debug("io_limits = %f, bps_iorate = %f", io_limits, bps_iorate)
+        if bps_iorate > io_limits:
+           raise error.TestFail('Throughput has exceeded threshold.')
         else:
            logging.info("The limit criteria is met...")
     finally:
